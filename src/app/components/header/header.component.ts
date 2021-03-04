27e00ad6d07d2209f361bd8b5211f3657fccdd7f
Emile Cyimena <cyimena09@hotmail.com>
2021-03-04 12:47:03 +0100

    interface et design

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  disconnected = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
+    this.disconnected = !this.disconnected;
   }
 
 }
